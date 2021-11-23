c1f9b366bcdc1fca911ebbeeb3e974a102299125
asheikh <asim.sardar.sheikh@gmail.com>
2021-11-23 11:08:37 +0100

    Adding the database components to the app

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-from flask import Flask
+import sqlite3
+
+from flask import Flask, g
+
+DATABASE = "flaskr.db"
 
 app = Flask(__name__)
+
+app.config.from_object(__name__)
+
+def connect_db():
+    rv = sqlite3.connect(app.config["DATABASE"])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource("schema.sql", mode="r") as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+def get_db():
+    if not hasattr(g, "sqlite_db"):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, "sqlite_db"):
+        g.sqlite_db.close()
 
 @app.route('/')
 def index():
     return 'Hello, World'
-
-@app.route('/test')
-def test():
-    return 'Test working'
--- test_app.py
+++ test_app.py
@@ -1,6 +1,17 @@
+import pytest
 from pathlib import Path
 
 from app import app
+
+@pytest.fixture
+def client():
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    app.config["TESTING"] = True
+    app.config["DATABASE"] = BASE_DIR.joinpath("test.db")
+
+    init_db()
+    yield app.test_client()
+    init_db()
 
 def test_index():
     tester = app.test_client()
@@ -8,13 +19,6 @@ def test_index():
 
     assert resp.status_code == 200
     assert resp.data == b'Hello, World'
-
-def test_test():
-    tester = app.test_client()
-    resp = tester.get('/test', content_type="html/text")
-
-    assert resp.status_code == 200
-    assert resp.data == b'Test working'
 
 def test_database():
     assert Path("flaskr.db").is_file()
