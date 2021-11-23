6af3accd88cbae18f64259d9431b4ee335d808f4
asheikh <asim.sardar.sheikh@gmail.com>
2021-11-23 11:17:43 +0100

    getting fixtures ready for template testing

--- test_app.py
+++ test_app.py
@@ -1,21 +1,30 @@
 import pytest
 from pathlib import Path
 
-from app import app
+from app import app, init_db
 
 @pytest.fixture
 def client():
     BASE_DIR = Path(__file__).resolve().parent.parent
     app.config["TESTING"] = True
-    app.config["DATABASE"] = BASE_DIR.joinpath("test.db")
+    app.config["DATABASE"] = str(BASE_DIR.joinpath("test.db"))
 
     init_db()
     yield app.test_client()
     init_db()
 
-def test_index():
-    tester = app.test_client()
-    resp = tester.get('/', content_type="html/text")
+def login(client, username, password):
+    return client.post(
+            "/login", 
+            data=dict(username=username, password=password),
+            follow_redirects=True,
+    )
+
+def logout(client):
+    return client.get('/logout', follow_redirects=True) 
+
+def test_index(client):
+    resp = client.get('/', content_type="html/text")
 
     assert resp.status_code == 200
     assert resp.data == b'Hello, World'
