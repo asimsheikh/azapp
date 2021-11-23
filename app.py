d2dc8aad0e778a5935264786a7c741db25da1c1e
asheikh <asim.sardar.sheikh@gmail.com>
2021-11-23 13:59:10 +0100

    removed app to get back to baseline

--- app.py
+++ app.py
@@ -1,73 +1,7 @@
-import sqlite3
-from flask import Flask, g, render_template, request, session, flash, redirect, url_for, abort
-
-DATABASE = "flaskr.db"
-USERNAME = "admin"
-PASSWORD = "admin"
-SECRET_KEY = "change_me"
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config.from_object(__name__)
-
-def connect_db():
-    rv = sqlite3.connect(app.config["DATABASE"])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource("schema.sql", mode="r") as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-def get_db():
-    if not hasattr(g, "sqlite_db"):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-@app.teardown_appcontext
-def close_db(error):
-    if hasattr(g, "sqlite_db"):
-        g.sqlite_db.close()
 
 @app.route('/')
 def index():
-    db = get_db()
-    cur = db.execute('select * from entries order by id desc')
-    entries = cur.fetchall()
-    return render_template('index.html', entries=entries)
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('index'))
-    return render_template('login.html', error=error)
-
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect(url_for('index'))
-
-@app.route('/add', methods=['POST'])
-def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
-    db = get_db()
-    db.execute(
-            'insert into entries (title, text) values (?,?)',
-            [request.form['title'], request.form['text']]
-            )
-    db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('index'))
-
+    return render_template('index.html')
--- test_app.py
+++ test_app.py
@@ -1,62 +1,13 @@
 import pytest
 from pathlib import Path
 
-from app import app, init_db
+from app import app
 
 @pytest.fixture
 def client():
-    BASE_DIR = Path(__file__).resolve().parent.parent
-    app.config["TESTING"] = True
-    app.config["DATABASE"] = str(BASE_DIR.joinpath("test.db"))
-
-    init_db()
     yield app.test_client()
-    init_db()
-
-def login(client, username, password):
-    return client.post(
-            "/login", 
-            data=dict(username=username, password=password),
-            follow_redirects=True,
-    )
-
-def logout(client):
-    return client.get('/logout', follow_redirects=True) 
 
 def test_index(client):
     resp = client.get('/', content_type="html/text")
     assert resp.status_code == 200
 
-def test_database():
-    tester = Path("flaskr.db").is_file()
-    assert tester
-
-def test_empty_db(client):
-    resp = client.get('/')
-    assert b'No entries yet. Add some!' in resp.data
-
-def test_login_logout(client):
-    rv = login(client, app.config["USERNAME"], app.config["PASSWORD"])
-    assert b'You were logged in' in rv.data
-
-    rv = logout(client)
-    assert b'You were logged out' in rv.data
-
-    rv = login(client, app.config["USERNAME"] + "x", app.config["PASSWORD"])
-    assert b'Invalid username' in rv.data
-
-    rv = login(client, app.config["USERNAME"], app.config["PASSWORD"] + "x")
-    assert b'Invalid password' in rv.data
-
-def test_messages(client):
-    login(client, app.config["USERNAME"], app.config["PASSWORD"])
-    rv = client.post(
-            "/add",
-            data=dict(title="<Hello>", text="<strong>HTML</strong> allowed here"),
-            follow_redirects=True,
-    )
-    assert b'No entries here so far' not in rv.data
-    assert b'&alt;Hello&gt;' in rv.data
-    assert b'<strong>HTML</strong> allowed here' in rv.data
-
-    
